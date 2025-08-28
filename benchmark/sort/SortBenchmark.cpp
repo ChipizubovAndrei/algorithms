98a4562b87704884d5013342a351f286398d983f
Chipizubov Andrei <chipizubow.andrej@gmail.com>
2025-08-28 19:37:01 +0700

    added stl sort benchmark

--- benchmark/sort/SortBenchmark.cpp
+++ benchmark/sort/SortBenchmark.cpp
@@ -20,7 +20,15 @@ void BubbleSortBenchmark(benchmark::State& state) {
 	}
 }
 
-//BENCHMARK(QuickSortBenchmark)->Arg(10)->Arg(10000)->Arg(1000000);
-BENCHMARK(BubbleSortBenchmark)->Arg(10)->Arg(10000)->Arg(1000000);
+void STLSortBenchmark(benchmark::State& state) {
+	for (auto _ : state) {
+		std::vector<int> test = generateRandomVector(state.range(0));
+		std::sort(test.begin(), test.end());
+	}
+}
+
+BENCHMARK(QuickSortBenchmark)->Arg(10)->Arg(100)->Arg(10000);
+BENCHMARK(BubbleSortBenchmark)->Arg(10)->Arg(100)->Arg(10000);
+BENCHMARK(STLSortBenchmark)->Arg(10)->Arg(100)->Arg(10000);
 
 BENCHMARK_MAIN();
